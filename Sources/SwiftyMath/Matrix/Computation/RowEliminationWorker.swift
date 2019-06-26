be3c5ee4c7a66b38da15f014b1a6d3ffd40f437d
Taketo Sano <taketo1024@gmail.com>
2019-06-26 16:04:32 +0900

    RowElim trackHeadPos

--- Sources/SwiftyMath/Matrix/Computation/RowEliminationWorker.swift
+++ Sources/SwiftyMath/Matrix/Computation/RowEliminationWorker.swift
@@ -15,7 +15,10 @@ internal final class RowEliminationWorker<R: EuclideanRing>: Equatable {
     private var working: Table
     private var result : Table
     
-    init<S: Sequence>(size: (Int, Int), components: S) where S.Element == MatrixComponent<R> {
+    private var trackHeadPosition: Bool
+    var headPositions: [Int : Set<Int>] // [col : { rows having head at col }]
+    
+    init<S: Sequence>(size: (Int, Int), components: S, trackHeadPosition: Bool = false) where S.Element == MatrixComponent<R> {
         self.size = size
         self.working = components.group{ c in c.row }
             .mapValues { l in
@@ -24,22 +27,30 @@ internal final class RowEliminationWorker<R: EuclideanRing>: Equatable {
         }
         self.result = [:]
         result.reserveCapacity(working.count)
+        
+        self.trackHeadPosition = trackHeadPosition
+        if trackHeadPosition {
+            self.headPositions = working
+                .map{ (i, head) in (i, head.value.col) }
+                .group{ (_, j) in j }
+                .mapValues{ l in Set( l.map{ (i, _) in i } ) }
+        } else {
+            self.headPositions = [:]
+        }
     }
     
-    convenience init<n, m>(from matrix: Matrix<n, m, R>) {
-        self.init(size: matrix.size, components: matrix)
+    convenience init<n, m>(from matrix: Matrix<n, m, R>, trackHeadPosition: Bool = false) {
+        self.init(size: matrix.size, components: matrix, trackHeadPosition: trackHeadPosition)
     }
     
-    func headElement(row i: Int) -> (col: Int, value: R)? {
+    func headElement(_ i: Int) -> (col: Int, value: R)? {
         return working[i]?.value
     }
     
     @_specialize(where R == 𝐙)
-    func headElements(col j0: Int) -> [(row: Int, value: R)] {
-        return working.compactMap{ (i, head) -> (Int, R)? in
-            let (j, a) = head.value
-            return j == j0 ? (i, a) : nil
-        }.sorted{ (i, _) in i }
+    func headElements(ofCol j: Int) -> [(row: Int, value: R)] {
+        assert(trackHeadPosition)
+        return headPositions[j]?.map { i in (i, working[i]!.value.value) } ?? []
     }
     
     @_specialize(where R == 𝐙)
@@ -72,9 +83,17 @@ internal final class RowEliminationWorker<R: EuclideanRing>: Equatable {
     }
     
     func swapRows(_ i: Int, _ j: Int) {
-        let r0 = working[i]
-        working[i] = working[j]
-        working[j] = r0
+        if trackHeadPosition {
+            removeHeadPosition(i)
+            removeHeadPosition(j)
+        }
+
+        (working[i], working[j]) = (working[j], working[i])
+        
+        if trackHeadPosition {
+            updateHeadPosition(i)
+            updateHeadPosition(j)
+        }
     }
     
     @_specialize(where R == 𝐙)
@@ -85,6 +104,10 @@ internal final class RowEliminationWorker<R: EuclideanRing>: Equatable {
         
         guard var toHead = working[i2] else {
             fatalError("attempt to add into empty row: \(i2)")
+        }
+        
+        if trackHeadPosition {
+            removeHeadPosition(i2)
         }
         
         if fromHead.value.col < toHead.value.col {
@@ -137,6 +160,10 @@ internal final class RowEliminationWorker<R: EuclideanRing>: Equatable {
         
         let result = toHead.drop{ c in c.value == .zero } // possibly nil
         working[i2] = result
+        
+        if trackHeadPosition {
+            updateHeadPosition(i2)
+        }
     }
     
     func finished(row i: Int) {
@@ -161,6 +188,20 @@ internal final class RowEliminationWorker<R: EuclideanRing>: Equatable {
         working = result
         result = [:]
         result.reserveCapacity(working.count)
+    }
+    
+    private func removeHeadPosition(_ i: Int) {
+        guard let j = working[i]?.value.col else { return }
+        headPositions[j]!.remove(i)
+    }
+    
+    private func updateHeadPosition(_ i: Int) {
+        guard let j = working[i]?.value.col else { return }
+        if headPositions[j] == nil {
+            headPositions[j] = [i]
+        } else {
+            headPositions[j]!.insert(i)
+        }
     }
     
     static func identity(size n: Int) -> RowEliminationWorker<R> {
