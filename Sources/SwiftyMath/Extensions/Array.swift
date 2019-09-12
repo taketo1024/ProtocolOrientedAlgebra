560989c78d8c6e29d0f6284d79240f88095063ea
Taketo Sano <taketo1024@gmail.com>
2019-09-12 15:23:07 +0900

    add Array.parallelMap etc.

--- Sources/SwiftyMath/Extensions/Array.swift
+++ Sources/SwiftyMath/Extensions/Array.swift
@@ -106,3 +106,28 @@ extension Array where Element: Hashable {
         return { dict[$0] }
     }
 }
+
+extension Array {
+    public func parallelMap<T>(transform: (Element) -> T) -> [T] {
+        var result = ContiguousArray<T?>(repeating: nil, count: count)
+        return result.withUnsafeMutableBufferPointer { buffer in
+            DispatchQueue.concurrentPerform(iterations: buffer.count) { idx in
+                buffer[idx] = transform(self[idx])
+            }
+            return buffer.map { $0! }
+        }
+    }
+    
+    public func parallelFlatMap<T>(transform: @escaping ((Element) -> [T])) -> [T] {
+        return parallelMap(transform: transform).flatMap { $0 }
+    }
+    
+    public func parallelCompactMap<T>(transform: @escaping ((Element) -> T?)) -> [T] {
+        return parallelMap(transform: transform).compactMap { $0 }
+    }
+    
+    public func parallelFilter(predicate: @escaping ((Element) -> Bool)) -> [Element] {
+        return parallelCompactMap { e in predicate(e) ? e : nil }
+    }
+}
+
