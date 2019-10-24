96acc6428b13f32de8ccc61c8e2222e2dbf114ac
Taketo Sano <taketo1024@gmail.com>
2019-10-24 12:09:55 +0900

    Cache is thread-safe.

--- Sources/SwiftyMath/Util/Cache.swift
+++ Sources/SwiftyMath/Util/Cache.swift
@@ -6,15 +6,28 @@
 //  Copyright © 2017年 Taketo Sano. All rights reserved.
 //
 
+import Dispatch
+
 public final class Cache<T>: CustomStringConvertible {
-    public var value: T?
+    private var _value: T?
+    private let queue: DispatchQueue
     
     public init(_ value: T? = nil) {
-        self.value = value
+        self._value = value
+        self.queue = DispatchQueue(label: "Cache", qos: .userInteractive)
     }
     
     public static var empty: Cache<T> {
         Cache()
+    }
+    
+    public var value: T? {
+        get {
+            queue.sync{ _value }
+        }
+        set {
+            queue.sync{ _value = newValue }
+        }
     }
     
     public var hasValue: Bool {
@@ -22,33 +35,43 @@ public final class Cache<T>: CustomStringConvertible {
     }
     
     public func useCacheOrSet(_ initializer: () -> T) -> T {
-        if let v = value {
+        queue.sync {
+            if let v = _value {
+                return v
+            }
+            
+            let v = initializer()
+            _value = v
             return v
         }
-        
-        let v = initializer()
-        value = v
-        return v
     }
     
     public func clear() {
-        self.value = nil
+        queue.sync {
+            _value = nil
+        }
     }
     
     public func copy() -> Cache<T> {
-        Cache(value)
+        queue.sync {
+            Cache(_value)
+        }
     }
     
     public var description: String {
-        "Cache(\(value.map{ "\($0)" } ?? "-"))"
+        queue.sync {
+            "Cache(\(value.map{ "\($0)" } ?? "-"))"
+        }
     }
 }
 
 public final class CacheDictionary<K, T>: CustomStringConvertible where K: Hashable {
     private var dictionary: [K : T]
-    
+    private let queue: DispatchQueue
+
     public init(_ dictionary: [K : T] = [:]) {
         self.dictionary = dictionary
+        self.queue = DispatchQueue(label: "CacheDictionary", qos: .userInteractive)
     }
     
     public static var empty: CacheDictionary<K, T> {
@@ -56,29 +79,41 @@ public final class CacheDictionary<K, T>: CustomStringConvertible where K: Hasha
     }
     
     public subscript (key: K) -> T? {
-        get { dictionary[key] }
-        set { dictionary[key] = newValue }
+        get {
+            queue.sync { dictionary[key] }
+        }
+        set {
+            queue.sync { dictionary[key] = newValue }
+        }
     }
     
     public func useCacheOrSet(key: K, _ initializer: () -> T) -> T {
-        if let v = dictionary[key] {
+        queue.sync {
+            if let v = dictionary[key] {
+                return v
+            }
+            
+            let v = initializer()
+            dictionary[key] = v
             return v
         }
-        
-        let v = initializer()
-        dictionary[key] = v
-        return v
     }
     
     public func remove(key: K) {
-        dictionary.removeValue(forKey: key)
+        queue.sync {
+            let _ = dictionary.removeValue(forKey: key)
+        }
     }
     
     public func clear() {
-        self.dictionary = [:]
+        queue.sync {
+            self.dictionary = [:]
+        }
     }
     
     public var description: String {
-        "Cache(\(dictionary))"
+        queue.sync {
+            "Cache(\(dictionary))"
+        }
     }
 }
