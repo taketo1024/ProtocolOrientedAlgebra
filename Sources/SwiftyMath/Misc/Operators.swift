0e4177b45327b8a2c78b5315edc04a083d58bb9a
Taketo Sano <taketo1024@gmail.com>
2019-11-05 11:12:55 +0900

    add some operators.

--- Sources/SwiftyMath/Misc/Operators.swift
+++ Sources/SwiftyMath/Misc/Operators.swift
@@ -14,12 +14,20 @@ prefix operator √
 
 infix operator ./: MultiplicationPrecedence
 infix operator /%: MultiplicationPrecedence
+
 infix operator ∩: MultiplicationPrecedence
 infix operator ∪: MultiplicationPrecedence
+infix operator ⊂: ComparisonPrecedence
+infix operator ⊃: ComparisonPrecedence
+infix operator ∈: ComparisonPrecedence
+infix operator ∉: ComparisonPrecedence
+
+infix operator •: MultiplicationPrecedence
 infix operator ∘: MultiplicationPrecedence
 infix operator ×: MultiplicationPrecedence
+
 infix operator ⊕: MultiplicationPrecedence
 infix operator ⊗: MultiplicationPrecedence
+
 infix operator ∧: MultiplicationPrecedence
 infix operator ∨: MultiplicationPrecedence
-infix operator •: MultiplicationPrecedence
