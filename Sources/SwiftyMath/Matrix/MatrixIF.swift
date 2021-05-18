39b4865ae818d44ffc92eda6003f032276fa07c7
Taketo Sano <taketo1024@gmail.com>
2021-05-18 16:43:38 +0900

    merge MatrixIF_static into MatrixIF.

--- Sources/SwiftyMath/Matrix/MatrixIF.swift
+++ Sources/SwiftyMath/Matrix/MatrixIF.swift
@@ -195,6 +195,8 @@ public struct MatrixIF<Impl: MatrixImpl, n: SizeType, m: SizeType>: SetType {
     }
 }
 
+// MARK: Sparse Matrix
+
 extension MatrixIF where Impl: SparseMatrixImpl {
     public var numberOfNonZeros: Int {
         impl.numberOfNonZeros
@@ -205,6 +207,8 @@ extension MatrixIF where Impl: SparseMatrixImpl {
         impl.nonZeroComponents
     }
 }
+
+// MARK: Square Matrix
 
 extension MatrixIF where n == m { // n, m: possibly dynamic
     public var isInvertible: Bool {
@@ -224,7 +228,8 @@ extension MatrixIF where n == m { // n, m: possibly dynamic
     }
 }
 
-// ColVector
+// MARK: ColVector
+
 extension MatrixIF where m == _1 { // n: possibly dynamic
     public typealias ColInitializer = (Int, BaseRing) -> Void
     public init(size n: Int, initializer s: @escaping (ColInitializer) -> Void) {
@@ -261,7 +266,8 @@ extension MatrixIF where m == _1 { // n: possibly dynamic
     }
 }
 
-// RowVector
+// MARK: RowVector
+
 extension MatrixIF where n == _1 { // m: possibly dynamic
     public typealias RowInitializer = (Int, BaseRing) -> Void
     public init(size m: Int, initializer s: @escaping (RowInitializer) -> Void) {
@@ -295,5 +301,111 @@ extension MatrixIF where n == _1 { // m: possibly dynamic
     public static func •(_ left: Self, _ right: Self) -> BaseRing {
         assert(left.size == right.size)
         return (0 ..< left.size.rows).sum { i in left[i] * right[i] }
+    }
+}
+
+// MARK: Fixed-size Matrix
+
+extension MatrixIF: AdditiveGroup, Module, ExpressibleByArrayLiteral where n: StaticSizeType, m: StaticSizeType {
+    public typealias ArrayLiteralElement = BaseRing
+    
+    public static var size: MatrixSize {
+        (n.intValue, m.intValue)
+    }
+    
+    public init(initializer: @escaping (Initializer) -> Void) {
+        self.init(size: Self.size, initializer: initializer)
+    }
+    
+    public init<S: Sequence>(grid: S) where S.Element == BaseRing {
+        self.init(size: Self.size, grid: grid)
+    }
+    
+    public init<S: Sequence>(entries: S) where S.Element == MatrixEntry<BaseRing> {
+        self.init(size: Self.size, entries: entries)
+    }
+    
+    public init(arrayLiteral elements: ArrayLiteralElement...) {
+        self.init(grid: elements)
+    }
+
+    public static var zero: Self {
+        self.zero(size: Self.size)
+    }
+    
+    public static var identity: Self {
+        self.identity(size: Self.size)
+    }
+
+    public static func scalar(_ a: BaseRing) -> Self {
+        self.init(Impl.scalar(size: Self.size, value: a))
+    }
+
+    public static func unit(_ i: Int, _ j: Int) -> Self {
+        self.unit(size: Self.size, at: (i, j))
+    }
+    
+    public static func diagonal(_ entries: BaseRing...) -> Self {
+        self.init { setEntry in
+            entries.enumerated().forEach { (i, a) in
+                setEntry(i, i, a)
+            }
+        }
+    }
+}
+
+// MARK: Fixed-size Square Matrix
+
+extension MatrixIF: Multiplicative, Monoid, Ring where n == m, n: StaticSizeType {
+    public init(from a : 𝐙) {
+        self.init(Impl.scalar(size: Self.size, value: BaseRing.init(from: a)))
+    }
+    
+    public var isInvertible: Bool {
+        impl.isInvertible
+    }
+    
+    public var inverse: Self? {
+        impl.inverse.flatMap{ .init($0) }
+    }
+    
+    public var determinant: BaseRing {
+        impl.determinant
+    }
+
+    public var trace: BaseRing {
+        impl.trace
+    }
+}
+
+// MARK: Fixed-size ColVector
+
+extension MatrixIF where n: StaticSizeType, m == _1 {
+    public init(initializer s: @escaping (ColInitializer) -> Void) {
+        self.init { setEntry in
+            s { (i, a) in
+                setEntry(i, 0, a)
+            }
+        }
+    }
+}
+
+// MARK: Fixed-size RowVector
+
+extension MatrixIF where n == _1, m: StaticSizeType {
+    public init(initializer s: @escaping (RowInitializer) -> Void) {
+        self.init { setEntry in
+            s { (j, a) in
+                setEntry(0, j, a)
+            }
+        }
+    }
+}
+
+// MARK: 1x1 Matrix
+
+extension MatrixIF where n == m, n == _1 {
+    public var asScalar: BaseRing {
+        self[0, 0]
     }
 }
