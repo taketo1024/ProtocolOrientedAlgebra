de9c629e7525b2e22c86e1b6007c5f1c0cbb03ef
Taketo Sano <taketo1024@gmail.com>
2021-05-28 22:10:31 +0900

    improve Permutations.

--- Tests/SwmCoreTests/Combinatorics/PermutationTests.swift
+++ Tests/SwmCoreTests/Combinatorics/PermutationTests.swift
@@ -48,4 +48,42 @@ class PermutationTests: XCTestCase {
         let τ = S3(indices: 1,2,0)
         XCTAssertEqual(σ.asMatrix * τ.asMatrix, (σ * τ).asMatrix)
     }
+    
+    func testCyclic() {
+        let σ = S3.cyclic(2,0,1)
+        XCTAssertEqual(σ[2], 0)
+        XCTAssertEqual(σ[0], 1)
+        XCTAssertEqual(σ[1], 2)
+    }
+    
+    func testCyclicDecomposition() {
+        let n = 10
+        let σ = Permutation<anySize>(length: n, indices: 3,2,1,5,9,7,8,0,4,6)
+        let cyclics = σ.cyclicDecomposition
+        
+        XCTAssertEqual(cyclics.count, 3)
+        XCTAssertEqual(Set(cyclics.map{ $0.count}), [2, 4, 4])
+        
+        let τ = cyclics.map { c in
+            Permutation<anySize>.cyclic(length: n, indices: c)
+        }.reduce(Permutation<anySize>.identity(length: n)) {
+            $1 * $0
+        }
+        
+        XCTAssertEqual(σ, τ)
+    }
+    
+    func testTranspositionDecomposition() {
+        let n = 10
+        let σ = Permutation<anySize>(length: 10, indices: 3,2,1,5,9,7,8,0,4,6)
+        let trans = σ.transpositionDecomposition
+        
+        let τ = trans.map { t in
+            Permutation<anySize>.transposition(length: n, indices: t)
+        }.reduce(Permutation<anySize>.identity(length: n)) {
+            $1 * $0 
+        }
+        
+        XCTAssertEqual(σ, τ)
+    }
 }
