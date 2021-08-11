a52875f8e2ecb55338dee71d532d387763330e9a
Taketo Sano <taketo1024@gmail.com>
2021-08-12 08:57:05 +0900

    simplify Cache (no local locks).

--- Sources/SwmCore/Util/Cache.swift
+++ Sources/SwmCore/Util/Cache.swift
@@ -9,11 +9,8 @@
 import Dispatch
 
 public final class Cache<Key, Value>: ExpressibleByDictionaryLiteral, CustomStringConvertible where Key: Hashable {
-    
-    // access to storage and locks must be synchronized globally.
-    private let global = DispatchQueue(label: "Cache", qos: .userInteractive)
-    private var keyLocks: [Key : DispatchSemaphore] = [:]
     private var storage: [Key : Value]
+    private let queue = DispatchQueue(label: "Cache", qos: .userInteractive)
 
     public init(_ storage: [Key : Value] = [:]) {
         self.storage = storage
@@ -29,73 +26,39 @@ public final class Cache<Key, Value>: ExpressibleByDictionaryLiteral, CustomStri
     
     public subscript (key: Key) -> Value? {
         get {
-            local(key).sync {
-                get(key)
-            }
-        } set {
-            local(key).sync {
-                set(key, newValue)
-            }
+            queue.sync { storage[key] }
+        }
+        set {
+            queue.sync { storage[key] = newValue }
         }
     }
     
     public func getOrSet(key: Key, _ initializer: () -> Value) -> Value {
-        local(key).sync {
-            if let value = get(key) {
-                return value
-            }
-            
-            let value = initializer() // this might be time consuming.
-            
-            set(key, value)
-            
+        if let value = self[key] {
             return value
         }
+        
+        let value = initializer()
+        
+        self[key] = value
+        return value
     }
 
     public func remove(key: Key) {
-        global.sync {
+        queue.sync {
             let _ = storage.removeValue(forKey: key)
-            let _ = keyLocks.removeValue(forKey: key)
         }
     }
     
     public func clear() {
-        global.sync {
+        queue.sync {
             self.storage = [:]
-            self.keyLocks = [:]
         }
     }
     
     public var description: String {
-        "Cache\(storage)"
-    }
-    
-    private func local(_ key: Key) -> DispatchSemaphore {
-        global.sync { () -> DispatchSemaphore in
-            if let lock = keyLocks[key] {
-                return lock
-            } else {
-                let lock = DispatchSemaphore(value: 1)
-                keyLocks[key] = lock
-                return lock
-            }
+        queue.sync {
+            "Cache\(storage)"
         }
-    }
-    
-    private func get(_ key: Key) -> Value? {
-        global.sync { storage[key] }
-    }
-    
-    private func set(_ key: Key, _ value: Value?) {
-        global.sync { storage[key] = value }
-    }
-}
-
-private extension DispatchSemaphore {
-    func sync<Result>(_ block: () -> Result) -> Result {
-        wait()
-        defer { signal() }
-        return block()
     }
 }
